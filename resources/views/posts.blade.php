abaf54f7fb85ad235f10cd73651e413a936851bb
soumri nadhem <blendplay2@gmail.com>
2022-02-26 16:18:28 +0100

    css js fixed

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <script src="app.js"></script>
+    <title>My Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1>My first Post</h1>
+        <p>bla bla bla bla bla bla bla bla bla</p>
+    </article>
+
+    <article>
+        <h1>My Second Post</h1>
+        <p>bla bla bla bla bla bla bla bla bla</p>
+    </article>
+
+    <article>
+        <h1>My Third Post</h1>
+        <p>bla bla bla bla bla bla bla bla bla</p>
+    </article>
+</body>
+
+</html>
